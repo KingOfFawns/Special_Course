3abdf2b38aead15d1a62a1bd26fee40d94628a15
KingOfFawns <fister>
2017-06-07 15:16:35 +0200

    Digit Span test V0 is done.  Started data storage.
    Got to finish AppControl.cs.
    
    https://unity3d.com/learn/tutorials/topics/scripting/persistence-saving-and-loading-data

---
+++ Special Course/Assets/Scripts/AppControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class AppControl : MonoBehaviour {
+
+	public static AppControl control;
+
+	public int DigitSpan_DigitLength = 3;
+
+	// Use this for initialization
+	void Awake () {
+		if (control == null) {
+			DontDestroyOnLoad (gameObject);
+			control = this;
+		} else if (control != this) {
+			Destroy (gameObject);
+		} 
+	}
+
+	public void Save(){
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/appData.dat");
+
+		// Data to store
+		AppData data = new AppData();
+		data.DigitSpan_DigitLength = DigitSpan_DigitLength;
+
+		// Store data
+		bf.Serialize (file, data);
+		file.Close();
+	}
+
+	public void Load(){
+		if (File.Exists (Application.persistentDataPath + "/appData.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/appData-dat", FileMode.Open);
+			AppData data = (AppData)bf.Deserialize (file);
+			file.Close ();
+
+			// Load data
+			DigitSpan_DigitLength = data.DigitSpan_DigitLength;
+		}
+	}
+}
+
+[Serializable]
+class AppData {
+	public int DigitSpan_DigitLength;
+}
---
+++ Special Course/Assets/Scripts/DigitSpan_Controller.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DigitSpan_Controller : MonoBehaviour {
+
+	public Text digitText;
+	public Image check;
+	public GameObject endCanvas;
+	public Text shownText;
+	public Text correctText;
+
+	private string digitNumber = "";
+	private string inputNumber = "";
+	private bool active = false;
+
+	private int correctSequences = 0;
+	private int numOfSequences = 0;
+
+	// Use this for initialization
+	void Start () {
+		// Get digit length from local storage
+
+		StartCoroutine(NextNumber ());
+
+		StartCoroutine(Timer());
+	}
+
+	IEnumerator Timer(){
+		// The timer waits for 60 seconds
+		yield return new WaitForSeconds (60f);
+
+		// Activate canvas representiong the end of the test
+		// plus show motivation scores
+		endCanvas.SetActive (true);
+		shownText.text = numOfSequences.ToString();
+		correctText.text = correctSequences.ToString ();
+
+		// Calculate data to be stored
+
+
+		// Wait for 3 seconds before transitioning to the next test
+		yield return new WaitForSeconds (3f);
+		// Go to next test
+		Debug.Log("Transition");
+	}
+
+	public void InputDigit(string digit){
+		if (!active) {
+
+			digitText.text = digit;
+
+			inputNumber = inputNumber + digit;
+
+			if (inputNumber.Length == digitNumber.Length) {
+
+				if (inputNumber == digitNumber) {
+					check.sprite = Resources.Load<Sprite>("checkmark");
+					correctSequences++;
+				} else {
+					check.sprite = Resources.Load<Sprite>("failure");
+				}
+
+				StartCoroutine (ResetCheck ());
+
+				inputNumber = "";
+				StartCoroutine(NextNumber ());
+			}
+		}
+	}
+
+	IEnumerator ResetCheck(){
+		// Wait 0.3 seconds, then remove checkmark/X
+		yield return new WaitForSeconds (0.3f);
+		check.sprite = Resources.Load<Sprite> ("None"); 
+	}
+
+	IEnumerator NextNumber(){
+		digitText.text = "";
+
+		yield return new WaitForSeconds (0.3f);
+
+		UpdateNumber ();
+	}
+
+	void UpdateNumber(){
+		numOfSequences++;
+
+		// Calculate number
+		digitNumber = "";
+		// i should be changed accordingly to stored data
+		for (int i = 0; i < 3; i++) {
+			int ran = Random.Range (1, 10);
+
+			digitNumber = digitNumber + ran.ToString ();
+		}
+
+		Debug.Log (digitNumber);
+
+		StartCoroutine (ShowDigits (digitNumber));
+
+	}
+
+	IEnumerator ShowDigits(string number){
+		active = true;
+
+		int length = number.Length;
+
+		for (int i = 0; i < length; i++) {
+			char digit = number [i];
+
+			digitText.text = digit.ToString();
+
+			yield return new WaitForSeconds (1);
+
+			digitText.text = "";
+
+			if (i != length - 1) {
+				yield return new WaitForSeconds (0.3f);
+			}
+		}
+
+		active = false;
+	}
+}
